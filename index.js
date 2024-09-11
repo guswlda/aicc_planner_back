9af2d709f77b554381e2e8d09c9b7e70605f03b3
guswlda <guswl71945@gmail.com>
2024-09-11 16:08:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://myplanner.guswldaiccproject.com',
     credentials: true,
   })
 );
