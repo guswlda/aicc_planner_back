bb79ce5e1c245b98545763ad1b6e5f9393493947
gitchan22 <chanye10@naver.com>
2024-09-12 18:35:00 +0900

    origin cors commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     // origin: 'http://localhost:3000',
-    origin: 'https://myplanner.guswldaiccproject.com/',
+    origin: 'https://myplanner.guswldaiccproject.com',
     credentials: true,
   })
 );
