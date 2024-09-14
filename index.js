3a83ab6b15ead1b833b801e8cfeedd28adca9ac2
guswlda <guswl71945@gmail.com>
2024-09-14 19:57:35 +0900

    http origin commit

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    // origin: 'http://localhost:3000',
-    origin: 'https://myplanner.guswldaiccproject.com',
+    origin: 'http://localhost:3000',
+    // origin: 'https://myplanner.guswldaiccproject.com',
     credentials: true,
   })
 );
